c326581f5152a4fa5d3166198574c55f0e2c1690
v.molotkov <v.molotkov@grohold.ru>
2023-05-10 10:37:32 +0300

    Fix main

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from time import sleep
 
 
 def main():
-    while True:
-        print('Hello dodik')
-        sleep(2)
+    # while True:
+    print('Hello dodik')
+        # sleep(2)
 
 
 if __name__ == "__main__":
